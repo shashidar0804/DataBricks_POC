f8265dd94ad758c537cc480d1829c924125c1b35
shashidar0804 <shashidar0804@gmail.com>
2024-11-28 09:44:46 +0200

    Create test_dummy.py

---
+++ tests/test_dummy.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+    """Dummy test."""
+
+    def test_dummy(self):
+        """Test, dummy is dummy."""
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
